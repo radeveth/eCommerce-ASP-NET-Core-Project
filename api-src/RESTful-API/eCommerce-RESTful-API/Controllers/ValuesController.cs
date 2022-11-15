da3d29e47c2bc97dafc380f0b5ed6256f6208f52
Radoslav Radev <88380154+calisthenicsGuy@users.noreply.github.com>
2022-11-15 11:40:54 +0200

    project initial setup

---
+++ api-src/RESTful-API/eCommerce-RESTful-API/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿namespace eCommerce_RESTful_API.Controllers
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult Get()
+        {
+            Dictionary<string, int> keyValuePairs = new Dictionary<string, int>()
+            {
+                { "Rado", 16 },
+            };
+            return new JsonResult(Ok(keyValuePairs.ToList()));
+        }
+    }
+}
