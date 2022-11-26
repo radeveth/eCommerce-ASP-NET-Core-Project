d8d84652a54db3903d9a7e55355f59ebca2d01ba
Radoslav Radev <88380154+calisthenicsGuy@users.noreply.github.com>
2022-11-27 01:18:09 +0200

    work on home page

---
+++ src/Ecommerce-Project/Ecommerce.Web/GlobalConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.Web
+{
+    public static class GlobalConstants
+    {
+        public static string Telephone = "0877475376";
+        public static string Email = "eshoppark@gmail.com";
+    }
+}
